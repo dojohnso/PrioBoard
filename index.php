f8b535d3cd052582f02d02b48ed76cfd384060e4
Doug <dojohnso@Valhalla.local>
2014-03-11 10:48:14 -0500

    no menu toggle for now

--- index.php
+++ index.php
@@ -35,20 +35,20 @@
         <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <!-- <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
-                    </button>
+                    </button> -->
                     <a class="navbar-brand" href="#">Retrospective 2.0</a>
                 </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="nav navbar-nav">
+                <!-- <div class="collapse navbar-collapse"> -->
+                    <!-- <ul class="nav navbar-nav"> -->
                         <!-- <li class="active"><a href="/?<?= $_SERVER['QUERY_STRING']; ?>">Home</a></li> -->
                         <!-- <li><a href="/submit?<?= $_SERVER['QUERY_STRING']; ?>">Submit</a></li> -->
-                    </ul>
-                </div>
+                    <!-- </ul> -->
+                <!-- </div> -->
             </div>
         </div>
 
