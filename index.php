6b9094299134d2da6f59023298d8773057886407
Doug <dojohnso@Valhalla.local>
2014-03-11 11:11:40 -0500

    no hash in brand link

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button> -->
-                    <a class="navbar-brand" href="#">Retrospective 2.0</a>
+                    <a class="navbar-brand" href="/?<?= $_SERVER['QUERY_STRING']; ?>">Retrospective 2.0</a>
                 </div>
                 <!-- <div class="collapse navbar-collapse"> -->
                     <!-- <ul class="nav navbar-nav"> -->
