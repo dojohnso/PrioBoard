b098ae0b5353820529c892ca063925e1c471096a
dojohnso <dojohnso@gmail.com>
2015-11-19 11:31:17 -0600

    navbar for mobile

--- index.php
+++ index.php
@@ -56,8 +56,14 @@ if ( $_GET['a'] == 'priogawd' )
             <div class="container">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="?<?= $_SERVER['QUERY_STRING']; ?>">PrioBoard</a>
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
                 </div>
-                <div class="collapse navbar-collapse">
+                <div class="collapse navbar-collapse" id="navbar">
                     <ul class="nav navbar-nav">
                         <li><a href="?new">New Code?</a></li>
                         <!-- <li>
